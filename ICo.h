7b939f8619d10e7cd70f196de05001cb972b1b06
mat1as75 <138666313+mat1as75@users.noreply.github.com>
2024-06-21 22:41:53 -0300

    Update ICo.h

--- ICo.h
+++ ICo.h
@@ -13,21 +13,21 @@
 #ifndef ICO_H
 #define ICO_H
 
-#include "../ICollection/collections/List.h"
-#include "../ICollection/collections/ListIterator.h"
-#include "../ICollection/collections/ListNode.h"
-#include "../ICollection/collections/OrderedDictionary.h"
-#include "../ICollection/collections/OrderedDictionaryEntry.h"
+#include "ICollection/collections/List.h"
+#include "ICollection/collections/ListIterator.h"
+#include "ICollection/collections/ListNode.h"
+#include "ICollection/collections/OrderedDictionary.h"
+#include "ICollection/collections/OrderedDictionaryEntry.h"
 //////////////////////////////////////////////////////////////
-#include "../ICollection/Integer.h"
-#include "../ICollection/String.h"
+#include "ICollection/Integer.h"
+#include "ICollection/String.h"
 //////////////////////////////////////////////////////////////
-#include "../ICollection/interfaces/ICollectible.h"
-#include "../ICollection/interfaces/ICollection.h"
-#include "../ICollection/interfaces/IDictionary.h"
-#include "../ICollection/interfaces/IIterator.h"
-#include "../ICollection/interfaces/IKey.h"
-#include "../ICollection/interfaces/OrderedKey.h"
+#include "ICollection/interfaces/ICollectible.h"
+#include "ICollection/interfaces/ICollection.h"
+#include "ICollection/interfaces/IDictionary.h"
+#include "ICollection/interfaces/IIterator.h"
+#include "ICollection/interfaces/IKey.h"
+#include "ICollection/interfaces/OrderedKey.h"
 
 
 #endif /* ICO_H */
